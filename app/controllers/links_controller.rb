c178e642c086ef4ea5f7a6abcb6eeb23b1fbd245
Vijaya Mishra <vijayam0421@gmail.com>
2025-09-30 18:39:14 +0000

    Add Link model

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,20 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.recent_first
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path, notice: "Link created successfully."
+    else
+        @links = Link.recent_first
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+private
+  def link_params
+    params.require(:link).permit(:url)
+  end
+end
